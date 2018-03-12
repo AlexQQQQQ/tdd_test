fd90550f4fa7ea5c64d533281754f8c8e2dadd25
AlexQQQQQ <alexander.kondrov@gmail.com>
2018-03-12 22:11:54 +0000

    trying to test for two users

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
@@ -7,6 +8,22 @@ def home_page(request):
     #if request.method == 'POST':
     #   return HttpResponse(request.POST['item_text'])
     #return render(request, 'home.html') 
-    return render(request, 'home.html', {
-           'new_item_text': request.POST.get('item_text', ''),
-    })             
+   # item = Item()
+   # item.text = request.POST.get('item_text', '')
+    #item.save()
+    if request.method == 'POST':
+       Item.objects.create(text=request.POST['item_text'])
+       return redirect('/') 
+
+#       new_item_text = request.POST['item_text']
+#       Item.objects.create(text=new_item_text)
+#       
+#    else:
+#       new_item_text = ''
+
+    items = Item.objects.all()   
+    return render(request, 'home.html', {'items': items})
+
+#, {
+  #         'new_item_text': new_item_text
+  #  })             
