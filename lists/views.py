d60e3f8fa3a51ecb8851560572c51bba555ddafa
AlexQQQQQ <alexander.kondrov@gmail.com>
2018-03-13 19:42:49 +0000

    commit

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List 
 
 # Create your views here.
 def home_page(request):
@@ -11,9 +11,9 @@ def home_page(request):
    # item = Item()
    # item.text = request.POST.get('item_text', '')
     #item.save()
-    if request.method == 'POST':
-       Item.objects.create(text=request.POST['item_text'])
-       return redirect('/') 
+    #if request.method == 'POST':
+       #Item.objects.create(text=request.POST['item_text'])
+       #return redirect('/lists/the-only-list-in-the-world/') 
 
 #       new_item_text = request.POST['item_text']
 #       Item.objects.create(text=new_item_text)
@@ -21,9 +21,17 @@ def home_page(request):
 #    else:
 #       new_item_text = ''
 
-    items = Item.objects.all()   
-    return render(request, 'home.html', {'items': items})
+ #   items = Item.objects.all()   
+    return render(request, 'home.html')
 
 #, {
   #         'new_item_text': new_item_text
   #  })             
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items':items})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list = list_) 
+    return redirect('/lists/the-only-list-in-the-world/')
