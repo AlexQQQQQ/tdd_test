885af69cc18c93a6100ae08d4140b3faff987c8c
AlexQQQQQ <alexander.kondrov@gmail.com>
2018-03-19 22:20:14 +0000

    hack ALLOWED_HOSTS to be *

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,23 +1,28 @@
 #from django.test import LiveServerTestCase
+import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
-from selenium.common.exceptions import WebDriverException
-import os
+#import unittest
+
+
 
 MAX_WAIT = 10
 
 #class NewVisitorTest(LiveServerTestCase):
 class NewVisitorTest(StaticLiveServerTestCase):
 
-    def setup(self):
+    def setUp(self):
         self.browser = webdriver.Firefox()
         staging_server = os.environ.get('STAGING_SERVER')
+        print('hello world')  
         if staging_server:
            self.live_server_url = 'http://' + staging_server   
 
+    def tearDown(self):
+        self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -35,13 +40,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
    
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        #browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
-    
+   # def setUp(self):
+   #     self.browser = webdriver.Firefox()
+   #     #browser = webdriver.Firefox()
+   
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
